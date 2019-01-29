738cee53d4f6456cd1dfa724725a70e6fed68e35
Liewe Gutter <lgutter@student.codam.nl>
2019-01-29 21:42:19 +0100

    Corrected names in bzero tests and uncommented NULL string test

--- libft_part1_tests/ft_BZero.m
+++ libft_part1_tests/ft_BZero.m
@@ -34,7 +34,7 @@
 	ft_bzero(str2, len);
 	while (c < len)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
@@ -59,12 +59,12 @@
 	ft_bzero(str2, len);
 	while (c < 10)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
 
-- (void)testMemSetWith0TimesTerm0 {
+- (void)testBZeroWith0TimesTerm0 {
 	unsigned char *str1 = (unsigned char*)malloc(0);
 	unsigned char *str2 = (unsigned char*)malloc(0);
 	int	c = 0;
@@ -74,12 +74,12 @@
 	ft_bzero(str2, len);
 	while (c < len)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
 
-- (void)testMemSetWith10Timesa {
+- (void)testBZeroWith10Timesa {
 	unsigned char *str1 = (unsigned char*)malloc(10);
 	unsigned char *str2 = (unsigned char*)malloc(10);
 	int	c = 0;
@@ -98,12 +98,12 @@
 	ft_bzero(str2, len);
 	while (c < len)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
 
-- (void)testMemSetWith10TimesNewLine {
+- (void)testBZeroWith10TimesNewLine {
 	unsigned char *str1 = (unsigned char*)malloc(10);
 	unsigned char *str2 = (unsigned char*)malloc(10);
 	int	c = 0;
@@ -122,13 +122,13 @@
 	ft_bzero(str2, len);
 	while (c < len)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
 
 
-- (void)testMemSetWith5TimesaInEmptyString {
+- (void)testBZeroWith5TimesaInEmptyString {
 	unsigned char *str1 = (unsigned char*)malloc(0);
 	unsigned char *str2 = (unsigned char*)malloc(0);
 	int	c = 0;
@@ -147,42 +147,25 @@
 	ft_bzero(str2, len);
 	while (c < len)
 	{
-		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
+		XCTAssert(str1[c] == str2[c], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c, str2[c], str1[c]);
 		c++;
 	}
 }
 
-//- (void)testMemSetWith10Times1InIntArr {
-//	int *str1 = (int *)malloc(sizeof(int) * 10);
-//	int *str2 = (int *)malloc(sizeof(int) * 10);
-//	int	c = 1;
-//	size_t len = (sizeof(int) * 10);
-//	int *check1;
-//	int *check2;
-//	check1 = memset(str1, c, len);
-//	check2 = ft_memset(str2, c, len);
-//	c = 0;
-//	XCTAssert(check1 == str1 && check2 == str2, @"return value 1 is %p, input 1 %p, return value 2 is %p, input 2 %p.", str1, check1, str2, check2);
-//	while (c < len)
-//	{
-//		XCTAssert(str1[c] == str2[c], @"ft_memset does not match memset at pos %i, returned %i instead of %i", c, str2[c], str1[c]);
-//		c++;
-//	}
-//}
+//WILL BREAK REAL AND FT BZERO.
+- (void)testBZeroWithVoidStrings {
+	int	c1 = 0;
+	size_t len1 = 10;
+	int	c2 = 0;
+	size_t len2 = 10;
 
-//- (void)testMemSetWithVoidStrings {
-//	int	c1 = 0;
-//	size_t len1 = 10;
-//	int	c2 = 0;
-//	size_t len2 = 10;
-//
-//	memset(NULL, c, len);
-//	ft_memset(NULL, c2, len2); //Will it break? YES
-//	while (c1 < len1)
-//	{
-//		XCTAssert(str1[c1] == str2[c1], @"ft_memset does not match memset at pos %i, returned %c instead of %c", c1, str2[c1], str1[c1]);
-//	}
-//}
+	bzero(NULL, c, len);
+	ft_bzero(NULL, c2, len2); //Will it break? YES
+	while (c1 < len1)
+	{
+		XCTAssert(str1[c1] == str2[c1], @"ft_bzero does not match bzero at pos %i, returned %c instead of %c", c1, str2[c1], str1[c1]);
+	}
+}
 
 
 @end
