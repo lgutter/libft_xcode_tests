3741b1a9964fd2ac080ab4c05cdb01a2955683db
Liewe Gutter <lgutter@student.codam.nl>
2019-02-01 20:09:32 +0100

    Changed order of tests for atoi to clarify tests outside of int range
    Changed order of tests for atoi to clarify tests outside of int range.
    Since atoi is only meant to handle ints, the importance of these tests is debatable. But if you want to reproduce the complete behaviour of the unix implementation of atoi, these tests should all pass.

--- Part 1 Tests/ft_AToI.m
+++ Part 1 Tests/ft_AToI.m
@@ -75,6 +75,107 @@
 	XCTAssert(ret1 == ret2, @"ft_atoi failed with minimum integer. Expected %i, returned %i", ret1, ret2);
 }
 
+- (void)testWithWhitespaceBeforeSimpleNumber {
+	char str[] = " 	    			\v \n 123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithPlusSignBeforeSimpleNumber {
+	char str[] = "+123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with a plus sign before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithPlusSignAndMinusSignBeforeSimpleNumber {
+	char str[] = "+-123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with a plus and minus sign before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithTwoPlusSignsBeforeSimpleNumber {
+	char str[] = "++123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with two plus signs before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithTwoMinusSignsBeforeSimpleNumber {
+	char str[] = "--123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with two minus signs before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithWhitespaceInteruptingSimpleNumber {
+	char str[] = "123	456";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace interupting simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithWhitespaceAfterSimpleNumber {
+	char str[] = "123 	    			\v \n ";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace after a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithLettersBeforeSimpleNumber {
+	char str[] = "abc123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithLettersAfterSimpleNumber {
+	char str[] = "123abc";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters after a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+- (void)testWithExtendedAsciiPlusWhiteSpaceBeforeSimpleNumber {
+	char *str = "\xe3 123";
+	int	ret1;
+	int ret2;
+
+	ret1 = atoi(str);
+	ret2 = ft_atoi(str);
+	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters before a simple number. Expected %i, returned %i", ret1, ret2);
+}
+
+//THE NEXT TESTS ARE FOR NUMBERS OUTSIDE THE RANGE OF INT.
 - (void)testWithNumberOverMaxInt{
 	char str[] = "7223372036854775807";
 	int	ret1;
@@ -213,106 +314,6 @@
 	ret1 = atoi(str);
 	ret2 = ft_atoi(str);
 	XCTAssert(ret1 == ret2, @"ft_atoi failed with value over max long. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithWhitespaceBeforeSimpleNumber {
-	char str[] = " 	    			\v \n 123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithPlusSignBeforeSimpleNumber {
-	char str[] = "+123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with a plus sign before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithPlusSignAndMinusSignBeforeSimpleNumber {
-	char str[] = "+-123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with a plus and minus sign before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithTwoPlusSignsBeforeSimpleNumber {
-	char str[] = "++123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with two plus signs before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithTwoMinusSignsBeforeSimpleNumber {
-	char str[] = "--123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with two minus signs before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithWhitespaceInteruptingSimpleNumber {
-	char str[] = "123	456";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace interupting simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithWhitespaceAfterSimpleNumber {
-	char str[] = "123 	    			\v \n ";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with whitespace after a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithLettersBeforeSimpleNumber {
-	char str[] = "abc123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters before a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithLettersAfterSimpleNumber {
-	char str[] = "123abc";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters after a simple number. Expected %i, returned %i", ret1, ret2);
-}
-
-- (void)testWithExtendedAsciiPlusWhiteSpaceBeforeSimpleNumber {
-	char *str = "\xe3 123";
-	int	ret1;
-	int ret2;
-
-	ret1 = atoi(str);
-	ret2 = ft_atoi(str);
-	XCTAssert(ret1 == ret2, @"ft_atoi failed with letters before a simple number. Expected %i, returned %i", ret1, ret2);
 }
 
 @end
